a2b9163a776c5c9c7eb3c71adc75e6bb5c915812
subo2002-Laptop <subocashell@gmail.com>
2026-03-28 17:07:25 +0000

    Had to update ZSMath so types agree with project that uses both...

---
+++ .zig-cache/o/98506c784f42ee96d3b6ada3badaeba4/dependencies.zig
@@ -0,0 +1,12 @@
+pub const packages = struct {
+    pub const @"ZSMath-0.0.0-xSatLphxAACTEntvdSUciRcxxr2Jr-WbYD1OGmF0yfca" = struct {
+        pub const build_root = "C:\\Users\\suboc\\AppData\\Local\\zig\\p\\ZSMath-0.0.0-xSatLphxAACTEntvdSUciRcxxr2Jr-WbYD1OGmF0yfca";
+        pub const build_zig = @import("ZSMath-0.0.0-xSatLphxAACTEntvdSUciRcxxr2Jr-WbYD1OGmF0yfca");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+        };
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "ZSMath", "ZSMath-0.0.0-xSatLphxAACTEntvdSUciRcxxr2Jr-WbYD1OGmF0yfca" },
+};
