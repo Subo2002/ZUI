56d326b9e13713285974714a8f0871126e1f866f
subo2002-Laptop <subocashell@gmail.com>
2026-04-04 17:22:57 +0100

    ZSMath Update

---
+++ .zig-cache/o/b77ac8ef03aae74a2157a325c086b7c7/dependencies.zig
@@ -0,0 +1,12 @@
+pub const packages = struct {
+    pub const @"ZSMath-0.0.0-xSatLgWRAAAWPZxQiS-9urctZX5COnUIZXv5tMc5bWfp" = struct {
+        pub const build_root = "C:\\Users\\suboc\\AppData\\Local\\zig\\p\\ZSMath-0.0.0-xSatLgWRAAAWPZxQiS-9urctZX5COnUIZXv5tMc5bWfp";
+        pub const build_zig = @import("ZSMath-0.0.0-xSatLgWRAAAWPZxQiS-9urctZX5COnUIZXv5tMc5bWfp");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+        };
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "ZSMath", "ZSMath-0.0.0-xSatLgWRAAAWPZxQiS-9urctZX5COnUIZXv5tMc5bWfp" },
+};
